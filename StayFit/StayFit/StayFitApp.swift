ef44b1a254881721c4f6ed5f633ed0085664dca4
Rishabh lalwani <rishabhlalwani1048@gmail.com>
2023-04-03 22:26:46 +0530

    App Initialised

---
+++ StayFit/StayFit/StayFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayFitApp.swift
+//  StayFit
+//
+//  Created by Rishabh Lalwani on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct StayFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
